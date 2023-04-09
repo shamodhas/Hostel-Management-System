8a661774c4f7111dcea7408d9704874dbfccd273
shamodha-s-rathnamalala <shamodhasrathnamalala@gmail.com>
2023-04-09 14:32:55 +0530

    finalize

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -1,13 +1,20 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import lk.ijse.hms.bo.BoFactory;
+import lk.ijse.hms.bo.BoTypes;
 import lk.ijse.hms.bo.custom.RoomBO;
+import lk.ijse.hms.bo.custom.StudentBO;
 import lk.ijse.hms.bo.custom.impl.RoomBOImpl;
 import lk.ijse.hms.dao.DaoFactory;
 import lk.ijse.hms.dao.DaoTypes;
 import lk.ijse.hms.dao.SuperDAO;
+import lk.ijse.hms.dao.custom.QueryDAO;
 import lk.ijse.hms.dao.custom.UserDAO;
 import lk.ijse.hms.dto.CustomDTO;
+import lk.ijse.hms.dto.StudentDTO;
 import lk.ijse.hms.dto.UserDTO;
+import lk.ijse.hms.entity.Student;
+import lk.ijse.hms.util.FactoryConfiguration;
 import lk.ijse.hms.view.tm.UserTM;
 
 
@@ -21,6 +28,12 @@ import java.util.List;
 
 public class Tester {
     public static void main(String[] args) {
+//        StudentBO studentBO = BoFactory.getInstance().getBO(BoTypes.STUDENT);
+//        StudentDTO student = new StudentDTO();
+//        student.setStudentId("S0001");
+//        studentBO.deleteStudent(student);
 
+        QueryDAO queryDAO = DaoFactory.getInstance().getDAO(DaoTypes.QUERY);
+        queryDAO.findAllUnPaidReservationDetailsByStudentId("S0001", FactoryConfiguration.getInstance().getSession());
     }
 }
